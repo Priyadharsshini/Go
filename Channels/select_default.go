ef3380af38afaab8d5d112c34b9b189a6369c95b
Priyadharsshini <dharsshinisakrapani@gmail.com>
2022-01-05 14:51:08 +0530

    Create select_default.go

---
+++ Channels/select_default.go
@@ -0,0 +1,39 @@
+/*
+CHANNELS - SELECT STATEMENT
+
+DEFAULT CASE
+Default make the select case always non-blocking.
+If any of the select case has a non-blocking or value readily available, then that will be executed.If not default case will be executed.
+Won have any chance to block that routine and schedule another routine.
+
+*/
+
+package main
+
+import "fmt"
+
+func readvalue(c chan int) {
+	c <- 5
+}
+
+func read(c chan int) {
+	c <- 6
+}
+
+func main() {
+
+	c := make(chan int)
+	c1 := make(chan int)
+	go readvalue(c)
+	go read(c1)
+
+	select {
+	case num := <-c:
+		fmt.Println(num)
+	case num1 := <-c1:
+		fmt.Println(num1)
+	default:
+		fmt.Println("I won let block")
+	}
+
+}
